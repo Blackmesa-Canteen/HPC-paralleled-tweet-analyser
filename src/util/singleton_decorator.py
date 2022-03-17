8dba1ce165dccfc11fbd3950415a18de3a6dd339
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-03-18 09:25:44 +1100

    update singleton logic

--- src/util/singleton_decorator.py
+++ src/util/singleton_decorator.py
@@ -1,9 +1,9 @@
-def singleton(cls):
+def singleton(cls, *args, **kw):
     _instance = {}
 
     def get_instance():
         if cls not in _instance:
-            _instance[cls] = cls()
+            _instance[cls] = cls(*args, **kw)
         return _instance[cls]
 
     return get_instance
