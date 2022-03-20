17eba8d47c3b122b04f93f03c50d4b0d50d93c20
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-03-20 23:19:32 +1100

    增加点说明

--- src/util/math_util.py
+++ src/util/math_util.py
@@ -4,6 +4,8 @@
 # Is the point in the outer rectangle
 
 # TODO: 这里是你可能会用到的一些数学、几何的静态函数
+# 注意，点在矩形边缘也算作在这个矩形上，这是为了方便考虑边界情况设计的
+# 当一个点在两个矩形的公共边上，需用下方的几个函数，结合作业要求判断该点属于哪个矩形
 def is_point_in_box(poi, sbox, toler=0.0001):
     # sbox=[[x1,y1],[x2,y2]]
     if sbox[0][0] <= poi[0] <= sbox[1][0] and sbox[0][1] <= poi[1] <= sbox[1][1]:
