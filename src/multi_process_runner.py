777e99744893704ba2e76dbf8d76829252e3876d
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-03-21 14:05:58 +1100

    added to types of runner

---
+++ src/multi_process_runner.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     multi_process_runner
+   Description :  calc runner for multi processes
+   Author :       Xiaotian Li
+   date：          21/03/2022
+-------------------------------------------------
+"""
+__author__ = 'xiaotian li'
+
+from src.util.singleton_decorator import singleton
+
+
+@singleton
+class multi_process_runner:
+
+    def __init__(self):
+        pass
