4e7bdc29e339272709d7afc73efbe8367f425713
BlackMesa-Canteen <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-04-02 11:01:14 +1100

    try to clean up logic

--- src/main.py
+++ src/main.py
@@ -35,52 +35,47 @@ if __name__ == '__main__':
 
     comm = MPI.COMM_WORLD                                                      
     rank = comm.Get_rank()                                                     
-    size = comm.Get_size()      
-    '''
-    TODO: 命令行解析进程数
-    '''
-    PROCESS_NUM = 8
+    size = comm.Get_size()
 
     if rank == 0:
         starttime = datetime.datetime.now()
         twitter_json_parser =  TwitterJsonParser()
         total_rows = twitter_json_parser.get_total_rows()
-        send_data = get_interval(total_rows, PROCESS_NUM)
+        send_data = get_interval(total_rows, size)
         # print("[Rank: {0}] Total rows: {1}".format(rank, total_rows))
 
-    else:                                                                      
-        send_data = None
-
-    # Per-process except rank 0
-    recv_data = comm.scatter(send_data, root=0)
-
-    start_index = recv_data[0]
-    total_rows_per_process = recv_data[1]
-
-    # start_index = 0
-    # total_rows_per_process = 600000
-    pool = ThreadPoolHandler(start_index=start_index, total_rows_per_process=total_rows_per_process, 
-                                    test_thread_step=500, test_mode=True)
-
-    pool.launch('lang_calc')   
-    result = pool.collect_result()    
-    send_data = result
-    recv_data = comm.gather(send_data, root=0)
-
-    if rank == 0:
-
+        # TODO
         grid_parser = GridJsonParser()
-        table = LangCalcHandler.table_union(recv_data, grid_parser) 
+        table = LangCalcHandler.table_union(recv_data, grid_parser)
 
         for key in table.keys():
-                table[key][1] = len(table[key][1])
-                # add [:10] to get top 10
-                table[key][2] = list(sorted(table[key][2].items(), key=lambda x: x[1], reverse=True))
+            table[key][1] = len(table[key][1])
+            # add [:10] to get top 10
+            table[key][2] = list(sorted(table[key][2].items(), key=lambda x: x[1], reverse=True))
         LangCalcHandler.view(table)
 
         endtime = datetime.datetime.now()
 
         print(" Time Total: ", endtime - starttime)
+
+    else:
+        # Per-process bourder
+
+
+        start_index = recv_data[0]
+        total_rows_per_process = recv_data[1]
+
+        # start_index = 0
+        # total_rows_per_process = 600000
+        pool = ThreadPoolHandler(start_index=start_index, total_rows_per_process=total_rows_per_process,
+                                 test_thread_step=500, test_mode=False)
+
+        pool.launch('lang_calc')
+        result = pool.collect_result()
+        send_data = result
+        recv_data = comm.gather(send_data, root=0)
+
+
 
 
 
